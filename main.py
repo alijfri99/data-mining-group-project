c70d683edc25cfebf90e8f432eb7b8ebab5a6876
Ali <alijfri99@gmail.com>
2021-06-12 20:27:24 +0430

    Started working on the Data Extractor class.

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from selenium import webdriver
 from subject_finder import SubjectFinder
 from course_finder import CourseFinder
+from data_extractor import DataExtractor
 
 PATH = "C:\Program Files (x86)\chromedriver\chromedriver.exe"
 INITIAL_URL = "https://www.gla.ac.uk/coursecatalogue/browsebysubjectarea/"
 driver = webdriver.Chrome(PATH)
 
-subject_finder = SubjectFinder(driver, INITIAL_URL)
-subjects = subject_finder.find_subjects()
+# subject_finder = SubjectFinder(driver, INITIAL_URL)
+# subjects = subject_finder.find_subjects()
 
-course_finder = CourseFinder(driver, subjects)
-courses = course_finder.find_courses()
+# course_finder = CourseFinder(driver, subjects)
+# courses = course_finder.find_courses()
 
-print(courses)
-print(len(courses))
+data_extractor = DataExtractor(driver, ["https://www.gla.ac.uk/coursecatalogue/course/?code=COMPSCI5095"])
+data = data_extractor.extract_data()
+print(data)
