b1913fb609b2a16647cf6c382204919583eee6a4
Ali <alijfri99@gmail.com>
2021-06-13 11:29:39 +0430

    Changed the structure of the Data Extractor class.

--- data_extractor.py
+++ data_extractor.py
@@ -1,30 +1,10 @@
 class DataExtractor:
-    def __init__(self, driver, courses):
+    def __init__(self, driver, subjects):
         self.driver = driver
-        self.courses = courses
+        self.subjects = subjects
 
     def extract_data(self):
-        result = list()
-        for course in self.courses:
-            course_data = list()
-            self.driver.get(course)
-            course_data.append("University of Glasgow")
-            course_data.append("Glas")
-            course_data.append(self.extract_course_school())
-            course_data.append(self.extract_course_title())
-            course_data.append(self.extract_course_aims())
-
-            result.append(course_data)
-        return result
-
-    def extract_course_school(self):
-        school = self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/div/div/main/div[1]/ul/li[2]")
-        return school.text[8:]
-
-    def extract_course_title(self):
-        course_title = self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/div/div/main/div[1]/h2")
-        return course_title.text
-
-    def extract_course_aims(self):
-        course_aims = self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/div/div/main/div[1]/div[7]")
-        return course_aims.text
+        for subject in self.subjects:
+            self.driver.get(subject)
+            element = self.driver.find_element_by_xpath("//a[@href='JavaScript:printCatalogue()']")
+            element.click()
--- main.py
+++ main.py
@@ -7,12 +7,8 @@ PATH = "C:\Program Files (x86)\chromedriver\chromedriver.exe"
 INITIAL_URL = "https://www.gla.ac.uk/coursecatalogue/browsebysubjectarea/"
 driver = webdriver.Chrome(PATH)
 
-# subject_finder = SubjectFinder(driver, INITIAL_URL)
-# subjects = subject_finder.find_subjects()
+subject_finder = SubjectFinder(driver, INITIAL_URL)
+subjects = subject_finder.find_subjects()
 
-# course_finder = CourseFinder(driver, subjects)
-# courses = course_finder.find_courses()
-
-data_extractor = DataExtractor(driver, ["https://www.gla.ac.uk/coursecatalogue/course/?code=COMPSCI5095"])
-data = data_extractor.extract_data()
-print(data)
+data_extractor = DataExtractor(driver, subjects)
+data_extractor.extract_data()
